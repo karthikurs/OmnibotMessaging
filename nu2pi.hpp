305ac692a5c496ce2a08c0966c59d01aff75b260
Karthik Urs <ursk@umich.edu>
2020-07-23 14:54:52 -0400

    reordered message fields for byte alignment

--- nu2pi.hpp
+++ nu2pi.hpp
@@ -4,6 +4,12 @@
 #include <stdint.h>
 
 struct nucleoGeneralUpdate{
+    // IMU Readings
+    float gyro[3];
+    float accel[3];
+    float mag[3];
+    // Motor current
+    float curr[3];
     // a_delta, b_delta, c_delta represent encoder tick change.
     int8_t a_delta;
     int8_t b_delta;
@@ -12,12 +18,6 @@ struct nucleoGeneralUpdate{
     int8_t batt;
     // Error Code
     int8_t error;
-    // IMU Readings
-    float gyro[3];
-    float accel[3];
-    float mag[3];
-    // Motor current
-    float curr[3];
 };
 
 #endif
